8bf65178ff23c0391370f5401a5ec7b506c53312
Sandeep <67914237+disk41@users.noreply.github.com>
2022-08-11 23:05:31 +0530

    Add files via upload

---
+++ arr/biker_cross1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int bikerCross(int n){
+    int count=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            for(int k=i+1;k<n;k++){
+                if(arr[k]==1)
+                    count++;
+            }
+        }
+    }
+    printf("%d",count);
+}
+
+int main(){
+    int m;
+    //scanf("%d",&m);
+    bikerCross(m);
+    return 0;
+}
+    
